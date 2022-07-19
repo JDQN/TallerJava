fd8a2419f8864984a1ec38e22e678dfeb4769236
JDQN <jdquimbayo72@misena.edu.co>
2022-07-19 14:53:57 -0500

    Actualizacion de nuevos ejercicios

--- src/main/java/org/example/Seis/Seis.java
+++ src/main/java/org/example/Seis/Seis.java
@@ -6,5 +6,25 @@ public class Seis {
 		/*
 		* Realiza el ejercicio anterior usando un ciclo for.
 		*/
+
+		Integer numerosImpares = 1;
+		Integer numeroPar = 2;
+
+		System.out.println("Numeros impares del 1 al 100");
+
+		for(int i = 0; i < 100; i++) {
+			if(numerosImpares % 2 != 0) {
+				System.out.println("Numero Par: " +numerosImpares);
+			}
+			numerosImpares++;
+		}
+
+		System.out.println("Numeros pares del 1 al 100");
+		for(int i = 0; i < 100; i++) {
+			if(numeroPar % 2 == 0) {
+				System.out.println("Numero Impar: " +numeroPar);
+			}
+			numeroPar++;
+		}
 	}
 }
