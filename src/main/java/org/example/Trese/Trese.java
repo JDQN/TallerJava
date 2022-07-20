df84538d3d1b1c244cf443c14f85283efddea2b6
JDQN <jdquimbayo72@misena.edu.co>
2022-07-19 20:26:41 -0500

    Ejercicio 13 validamos fecha y hora actual

--- src/main/java/org/example/Trese/Trese.java
+++ src/main/java/org/example/Trese/Trese.java
@@ -1,2 +1,32 @@
-package org.example.Trese;public class Trese {
+package org.example.Trese;
+
+import java.util.Scanner;
+
+public class Trese {
+
+	/*
+	* Realizar un algoritmo que permita consulta la fecha y hora actual en el formato
+	* (AAAA/MM/DD) (HH:MM:SS)
+	*/
+
+	public static void main(String[] args) {
+		System.out.println("Escoje 1 para saver la fecha y hora actual y 2 para salir");
+		Scanner opcion = new Scanner(System.in);
+		int opciones = opcion.nextInt();
+
+		switch (opciones){
+			case 1:
+				System.out.println("La fecha actual es: " + java.time.LocalDate.now());
+				System.out.println("La hora actual es: " + java.time.LocalTime.now());
+				break;
+
+			case 2 :
+				System.out.println("Gracias por participar");
+				break;
+
+			default:
+				System.out.println("Porfavor escoja una obcion correcta");
+				break;
+		}
+	}
 }
