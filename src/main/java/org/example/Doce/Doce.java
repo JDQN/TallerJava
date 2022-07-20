78fb6e41bea1a77f655420db089481d295e99781
JDQN <jdquimbayo72@misena.edu.co>
2022-07-19 20:04:25 -0500

    Ejercicio 12 comparacion d e2 palabras si son ==

--- src/main/java/org/example/Doce/Doce.java
+++ src/main/java/org/example/Doce/Doce.java
@@ -1,2 +1,30 @@
-package org.example.Doce;public class Doce {
+package org.example.Doce;
+
+import java.util.Scanner;
+
+public class Doce {
+
+	public static void main(String[] args) {
+
+		/*
+		*  Pedir dos palabras por teclado, indicar si son
+		*  iguales, sino son iguales mostrar sus diferencias.
+		*/
+
+		System.out.println("Ingrese la primera palabra");
+		Scanner palabraUno = new Scanner(System.in);
+		String InputPalabraUno = palabraUno.nextLine();
+
+
+		System.out.println("Ingrese la segunda palabra");
+		Scanner palabraDos = new Scanner(System.in);
+		String InputPalabraDos = palabraDos.nextLine();
+
+		if(InputPalabraUno.equals(InputPalabraDos)){
+			System.out.println("Las palabras son iguales");
+		}else{
+			System.out.println("Las palabras son diferentes");
+		}
+		
+	}
 }
